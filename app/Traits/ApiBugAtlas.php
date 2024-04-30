739c9b308fdb787b8c28440c10280c3f87492b78
SparkoutBalaji <balaji.n@sparkouttech.com>
2024-04-30 10:21:02 +0530

    Http curl call works done, guzzleHttp package removed

--- app/Traits/ApiBugAtlas.php
+++ app/Traits/ApiBugAtlas.php
@@ -2,50 +2,18 @@
 
 namespace App\Traits;
 
-use GuzzleHttp\Client;
-use RequestException;
-
+use Illuminate\Support\Facades\Http;
 trait ApiBugAtlas
 {
     
     private $baseURL = 'https://api.bugatlas.com/v1';
 
-
     public function processApiResponse($endPoint, $body)
     {
-        $client = new Client([
-            'headers' => $this->getApiHeaders(),
-        ]);
-
-        // try {
-            // Make the POST request
-            $response = $client->post($this->baseURL . $endPoint, [
-                $body,
-            ]);
-            dd($response);
-            // Get the response body
-            $responseData = json_decode($response->getBody()->getContents(), true);
-            dd($responseData->body());
-            // Return the response data
-            return $responseData;
-        // } catch (RequestException $e) {
-        //     // Handle request exceptions
-        //     // Log the error or return a custom error response
-        //     return [
-        //         'error' => 'Request failed',
-        //         'message' => $e->getMessage(),
-        //     ];
-        // } catch (\Exception $e) {
-        //     // Handle other exceptions
-        //     return [
-        //         'error' => 'An error occurred',
-        //         'message' => $e->getMessage(),
-        //     ];
-        // }
-       
+        $response = Http::withHeaders($this->getApiHeaders())->post($this->baseURL.$endPoint, $body);
+        dd($response->body());
+        return $response;
     }
-
-
 
     private function getApiHeaders()
     {
